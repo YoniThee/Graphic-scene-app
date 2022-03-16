79dc54eba35775d0e38717f943caabdb82fe3cd5
shaydvis1 <dopletshay1@gmail.com>
2022-03-16 14:23:37 +0200

    end targil 2 tests

--- src/unittest/geomatries/CylinderTest.java
+++ src/unittest/geomatries/CylinderTest.java
@@ -4,18 +4,39 @@ import geometries.Cylinder;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 class CylinderTest {
     /*
      * Testing Cylinder
      * @author Shay Dopelt && Yheonatan Thee
      * */
-    @Test
-    public void testConstructor() {
-    }
+
 
     @Test
     /**Test method for {@link Cylinder.GetNormal}*/
     void testGetNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        Cylinder cylinder = new Cylinder(2, new Ray(new Point(0, 0, 0), new Vector(0, 0, 2)), 5);
+        // TC01: Normal test on the side of the cylinder
+        assertEquals( new Vector(0, 1, 0),cylinder.getNormal(new Point(0, 2, 2)),"ERROR: Normal test on the tube of the cylinder");
+
+
+        // TC02: Normal test on the top of the cylinder
+        assertEquals( new Vector(0, 0, 1), cylinder.getNormal(new Point(1, 0, 5)),"ERROR: Normal test on the top of the cylinder");
+        // TC03: Normal test on the bottom of the cylinder
+        assertEquals(new Vector(0, 0, 1), cylinder.getNormal(new Point(1, 1, 0)),"ERROR: Normal test on the bottom of the cylinder");
+
+        // ========== Boundary Values Tests ==================
+        // TC11: A test on the top of the cylinder
+        assertThrows(IllegalArgumentException.class, () -> {
+            cylinder.getNormal(new Point(0, 0, 5));
+        }, "ERROR: A test on the top of the cylinder");
+        // TC12: A test on the bottom of the cylinder
+        assertThrows(IllegalArgumentException.class, () -> {
+            cylinder.getNormal(new Point(0, 0, 0));
+        }, "ERROR: A test on the bottom of the cylinder");
     }
 }
--- src/unittest/geomatries/TubeTest.java
+++ src/unittest/geomatries/TubeTest.java
@@ -22,10 +22,27 @@ class TubeTest {
 
     void testGetNormal() {
         ///////////////////its still not true
-        Ray r = new Ray(new Point(1,2,3),new Vector(1,1,1));
-        Tube t = new Tube(3,r);
-        assertThrows(IllegalArgumentException.class, ()-> t.getNormal(new Point(1,2,3)),
-                " Point is versicle to the ray");
-        assertEquals(t.getNormal(new Point(1,2,3)),new Vector(5,5,5), "The normal of Tube is wrong");
+     //   Ray r = new Ray(new Point(1,2,3),new Vector(1,1,1));
+      //  Tube t = new Tube(3,r);
+      //  assertThrows(IllegalArgumentException.class, ()-> t.getNormal(new Point(1,2,3)),
+      //          " Point is versicle to the ray");
+        //assertEquals(t.getNormal(new Point(1,2,3)),new Vector(5,5,5), "The normal of Tube is wrong");
+
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: A simple case of a normal
+        Tube tube = new Tube(2, new Ray(new Point(0, 0, -5), new Vector(0, 0, 1)));
+        double sqrt3 = Math.sqrt(1d / 3);
+        assertEquals(new Vector(sqrt3, sqrt3, sqrt3).length(), tube.getNormal(new Point(1, 0, 0)).length(), 0.0001,
+                "ERROR: normal result is incorrect");
+
+        // ========== Boundary Values Tests ==================
+        // TC11: A Right angle with the ray source point
+        assertThrows(IllegalArgumentException.class, () -> {
+            Tube t = new Tube(2, new Ray(new Point(3, 4, 0), new Vector(2, -1, 0)));
+            t.getNormal(new Point(2, 2, 0));
+        }, "ERROR: A Right angle with the ray source point");
+
+
     }
 }
