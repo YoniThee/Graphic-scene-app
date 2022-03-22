5789158a5149c6cbf2efd050ec7e0e98858b63ab
shaydvis1 <dopletshay1@gmail.com>
2022-03-22 14:37:53 +0200

    Added tests and implement to plane and tryangle - find intersactions and tests - sending again

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -20,7 +20,26 @@ public class Cylinder extends Tube{
 
     @Override
     public Vector getNormal(Point point) {
-        return super.getNormal(point);
+       // return super.getNormal(point);
+
+        // find the vector on the lower base
+        Vector v1 = point.subtract(axisRay.getP0());
+        if (v1.dotProduct(axisRay.getDir()) == 0) { // the vectors is orthogonal to each other
+            if (v1.length() <= radius) { // the point is on the base of the cylinder
+                return axisRay.getDir();
+            }
+        }
+        // find the vector on the upper base
+        Vector v2 = axisRay.getDir().scale(axisRay.getDir().normalize().dotProduct(v1));
+        if (v1.normalize().equals(v2.normalize())) {
+            throw (new IllegalArgumentException("ERROR: Point is versicle to the ray"));
+        } else if (v1.equals(v2)) {
+            return axisRay.getDir();
+        } else {
+            Vector normal = v1.subtract(v2);
+            return normal.length() == radius ? normal.normalize() : axisRay.getDir();
+        }
+
     }
 
     @Override
