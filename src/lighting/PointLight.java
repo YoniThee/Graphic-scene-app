974e18de232b3d97bd770c2581d05ca602386851
shaydvis1 <dopletshay1@gmail.com>
2022-05-10 11:44:15 +0300

    PR07-  starting

--- src/lighting/PointLight.java
+++ src/lighting/PointLight.java
@@ -7,48 +7,48 @@ import primitives.Vector;
 
 public class PointLight extends Light implements LightSource{
     private Point position;
-    private Double3 kC, kL, kQ;
+    private Double kC, kL, kQ;
     protected PointLight(Color color) {
         super(color);
-        kC = new Double3(1.0);
-        kL = new Double3(0.0);
-        kQ = new Double3(0.0);
+        kC = 1.0;
+        kL = 0.0;
+        kQ = 0.0;
     }
 
     public PointLight(Color color, Point position) {
         super(color);
-        kC = new Double3(1.0);
-        kL = new Double3(0.0);
-        kQ = new Double3(0.0);
+        kC = 1.0;
+        kL = 0.0;
+        kQ = 0.0;
         this.position = position;
     }
 
-    public PointLight setKc(Double3 kC) {
+    public PointLight setKc(Double kC) {
         this.kC = kC;
         return this;
     }
 
-    public PointLight setKl(Double3 kL) {
+    public PointLight setKl(Double kL) {
         this.kL = kL;
         return this;
     }
 
-    public PointLight setKq(Double3 kQ) {
+    public PointLight setKq(Double kQ) {
         this.kQ = kQ;
         return this;
     }
 
     @Override
     public Color getIntensity(Point p) {
-        Color I0 = getIntensity();
-        Double3 d = new Double3(p.distance(p,position));
-        Double3 d2 = new Double3(p.distanceSquared(position));
-        Double3 ans = new Double3((kC.add( kL.product(d)) + kQ.product(d2)));
+        Color intensity = getIntensity();
+        double dSquared = p.distanceSquared(position);
+        double d = Math.sqrt(dSquared);
+        double ans = (kC + kL *d + kQ*dSquared);
         if(ans <1)
         {
             return this.getIntensity();
         }
-        return (I0.scale(1/(kC + kL *d + kQ*d2)));
+        return intensity.reduce(ans);
 
     }
 
